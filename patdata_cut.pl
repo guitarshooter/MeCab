01ed67719410fd53d7a61a0a07f2cd30ce952a04
shooter@office <10005239@j4002468.opt.olympus.co.jp>
2011-06-01 18:51:06 +0900

    特許データ分割

--- patdata_cut.pl
+++ patdata_cut.pl
@@ -8,19 +8,20 @@ while(@ARGV){
   my $file = shift @ARGV;
   my ($filename,$filepath,$filesuffix) = fileparse($file,$regex_suffix); 
   open(IN,"<$file");
-#  open(DATA,">$filename"."_dat.txt");
+  open(DATA,">$filename"."_dat.txt");
 
   $txt = do { local $/; <IN> };
   $txt =~ s/^↑//gm; #頭の矢印を削除
   $txt =~ s/^.+\(57\)【要約】//s; #文の頭から要約までを削除
-  $txt =~ s/【符号の説明】.+【代表図面】//s;
-  $txt =~ s/^【図.+】 //gm;
-  $txt =~ s/^【.+】$//gm; #【○○】のみ行を削除
+  #$txt =~ s/【符号の説明】.+【代表図面】//s;
+  $txt =~ s/【図面の簡単な説明】.+$//s; #【符号の説明】以下全部削除
+  $txt =~ s/フロントページの続き.+$//s; #上記がない場合、フロントページの続き以下を削除
+  $txt =~ s/^【選択図】.+$//gm;
   $txt =~ s/^\s$//gm; #先頭が改行
-  $txt =~ s/^──.+──$//gm; #先頭が改行
-  if($txt =~ /【手続補正書】/){
-    
-  }
-#  $txt =~ s/フロントページの続き.+$//s; #先頭が改行
-  print $txt;
+  $txt =~ s/^(?:\s|　)+//gm;#先頭スペース削除 
+  $txt =~ s/^(.*?)(?:\s|　)+$/$1/gm;#後ろスペース削除 
+  $txt =~ s/^──.*──//gm; #先頭が改行
+  $txt =~ s/^【.+?】(.*)$/$1/gm; #【○○】のみ行を削除
+  $txt =~ s/^\s*$//gm;#空行を削除
+  print DATA $txt;
 }
