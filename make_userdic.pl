495afd49955487fe4bd3e37789b8df324ab87c33
shooter@office <10005239@J4002468.local>
2011-06-24 08:58:55 +0900

    UniDicの原型をとるのをやめた。 make_userdic.plを追加。

---
+++ make_userdic.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use encoding 'utf8';
+use File::Basename;
+
+if(@ARGV != 2){die "Usage:$0 filename DictionaryName";}
+my ($inputfile,$dicname) = @ARGV;
+my $regex_suffix = qw/\.[^\.]+$/; #拡張子をのぞくための正規表現
+my ($filename,$filepath,$filesuffix) = fileparse($inputfile,$regex_suffix); 
+my $outputfile = "$filename".".csv";
+
+open(IN, "$inputfile");
+open(OUT, ">$outputfile");
+binmode OUT, ":utf8";        ##    <- こっちが正しい
+
+for(<IN>) {
+  chomp($_);
+  print OUT "$_,0,0,".max(-36000,-400 * (length^1.5)).",名詞,固有名詞,*,*,*,*,$_,*,*,$dicname,\n";
+}
+
+sub max {
+    my $comp = shift @_;
+    my $val  = shift @_;
+    my $max  = $comp;
+    if ( $comp <= $val ) {
+      $max = $val;
+    }
+    return int($max);
+}
+
