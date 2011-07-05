5456793bf731a98d1f85d89ab73a2177cba8f5c4
shooter@office <10005239@J4002468.local>
2011-07-05 17:38:13 +0900

    特許解析用にNAIST辞書から固有名詞を除去するスクリプトを追加。
    入力をNAIST辞書とすると、固有名詞を除去したCSVファイルを出力する。

---
+++ make_patdic.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use encoding 'utf8';
+use File::Basename;
+
+#if(@ARGV != 2){die "Usage:$0 filename DictionaryName";}
+my ($inputfile,$dicname) = @ARGV;
+my $regex_suffix = qw/\.[^\.]+$/; #拡張子をのぞくための正規表現
+my ($filename,$filepath,$filesuffix) = fileparse($inputfile,$regex_suffix); 
+#my $outputfile = "$filename"."_patdic.csv";
+
+open(IN, "$inputfile");
+open(OUT, ">$outputfile");
+binmode OUT, ":utf8";        ##    <- こっちが正しい
+
+for(<IN>) {
+#  chomp($_);
+  my @line = split(/,/,$_);
+  if ($line[5] ne "固有名詞") {
+#    print OUT $_;
+    print $_;
+  }
+}
