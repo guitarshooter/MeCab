23ce6efba4efc81c595cad32178c07dbc7eae534
shooter@office <10005239@J4002468.local>
2011-07-21 18:04:47 +0900

    専門用語作成時に出力した用語がどう分割されたか表示するように変更。

--- make_technicalterm.pl
+++ make_technicalterm.pl
@@ -8,6 +8,7 @@ use Unicode::Japanese;
 my $DELLSTR = "DELLWORD"; #削除辞書区別フラグ
 
 my %term_score;
+my %word_join;
 
 while(<>){
   my $txt = "";
@@ -20,6 +21,7 @@ while(<>){
   
   my $node_cnt = 0;
   my $delflg = 0;
+  my $node_word = "";
   $node = $mecab->parseToNode($txt);
   while ($node) {
     my $word     = $node->{'surface'};
@@ -29,6 +31,7 @@ while(<>){
     $nom_word = Unicode::Japanese->new($word)->h2z->get; #半角→全角に統一
     if ( $features[0] ne "BOS/EOS" ){
       $node_cnt++;
+      $node_word = $node_word." ".$word;
       if ($features[9] eq $DELLSTR){
         $delflg = 1;
       }
@@ -43,8 +46,9 @@ while(<>){
   }
   if (($node_cnt > 1 ) && ($delflg == 0 )){
     $term_score{$txt}+=$score;
+    $word_join{$txt}=$node_word;
   }
 }
-foreach my $word (sort { $term_score{$b} <=> $term_score{$a} } keys %term_score){
-  print "$word,$term_score{$word}\n";
+foreach my $wd (sort { $term_score{$b} <=> $term_score{$a} } keys %term_score){
+  print "$wd,$term_score{$wd},$word_join{$wd}\n";
 }
