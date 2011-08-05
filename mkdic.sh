35cc5ac47bb5c563a1794ec3e821d0f2ccb0e116
shooter@office <10005239@j4002468.opt.olympus.co.jp>
2011-08-05 16:29:43 +0900

    辞書作成スクリプトを変更。複数ファイル読み込み可能にした。

--- mkdic.sh
+++ mkdic.sh
@@ -3,13 +3,13 @@ BINDIR=/Users/10005239/Bin/MeCab
 TERMDIR=/Users/10005239/Bin/MeCab/Dictest/TermDic
 DICDIR=/usr/local/lib/mecab/dic/naist-pat/
 USERDIC=userdic.dic
-USERTXT=userdic.txt
 TERMTXT=maketerm.txt
 DELLTXT=delword.txt
 PHYSICTXT=physicdic.txt
 
 cd $TERMDIR
-perl $BINDIR/make_userdic.pl $PHYSICTXT PHYSICALWORD >userdic.csv
-perl $BINDIR/make_userdic.pl $TERMTXT PHYSICALWORD >>userdic.csv
-perl $BINDIR/make_userdic.pl $DELLTXT DELLWORD >>userdic.csv
+#perl $BINDIR/make_userdic.pl $PHYSICTXT PHYSICALWORD >userdic.csv
+perl $BINDIR/make_userdic.pl PHYSICALWORD $PHYSICTXT $TERMTXT >userdic.csv
+#perl $BINDIR/make_userdic.pl $TERMTXT PHYSICALWORD >>userdic.csv
+perl $BINDIR/make_userdic.pl DELLWORD $DELLTXT >>userdic.csv
 sudo /usr/local/libexec/mecab/mecab-dict-index -d $DICDIR -t utf-8 -f utf-8 -u ./userdic.dic userdic.csv
