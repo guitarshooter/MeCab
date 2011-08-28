824b3f30b71a73f3d421c50942ed6269b48fdd77
shooter <shooter@Luna.local>
2011-08-28 16:05:31 +0900

    コレスポンデンス分析用のファイル名を変更。

--- corresp.pl
+++ corresp.pl
@@ -1,11 +1,20 @@
 #!/usr/bin/perl
 use FindBin;
+use Cwd;
 
 my $bindir = $FindBin::Bin; #実行パス
+my $wd = Cwd::getcwd();
+print $bindir."\n";
 my $Rscript = "corresp.R"; #コレスポンデンス分析用Rスクリプト名
 my $outData_col = "corr_data_arrt.txt"; #出力ファイル名（文書名）
 my $outData_row = "corr_data_word.txt"; #出力ファイル名（単語）
-my $inputData = "arrt_word_tf.txt"; #入力ファイル名（文書×単語（TF値）ファイル）
+my $inputData = $wd."/"."arrt_words_tf_select.txt"; #入力ファイル名（文書×単語（TF値）ファイル）
+if( -f $inputData){
+  print "$inputData"." exist.\n";
+}else{
+  print "$inputData"." not exist."."\n";
+  exit;
+}
 
 #print ("R  --vanilla --slave < $bindir/$Rscript"); 
 
