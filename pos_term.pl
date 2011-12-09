8650f755c319608caf2a01e43d51f429ebd225a0
shooter@office <10005239@j4002468.opt.olympus.co.jp>
2011-12-09 15:45:22 +0900

    wordcount.pl を分割し形態素解析＆キーワード抽出のみを行うpos_term.plを作成

---
+++ pos_term.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+use MeCab;
+use Data::Dumper;
+use File::Basename;
+use TermExtract::MeCab;
+use Unicode::Japanese;
+
+my %filetitle;
+my $fileid=0;
+my $regex_suffix = qw/\.[^\.]+$/; #拡張子をのぞくための正規表現
+my $DELLSTR="DELLWORD"; #削除辞書区別フラグ
+
+while(@ARGV){
+  my $txt = "";
+  my $pos_str = "";
+  my $file = shift @ARGV;
+  print $file."...\n";
+  my ($filename,$filepath,$filesuffix) = fileparse($file,$regex_suffix); 
+  open(IN,"<$file");
+  open(TRM,">$filename"."_term.txt");
+  open(POS,">$filename"."_pos.txt");
+  $txt = do { local $/; <IN> };
+  unless($txt){
+    print "Notice:".$filename." is none...\n";
+    next;
+  }
+  $filetitle{$fileid}=$filename;
+  print CNT ","."$filename";
+  print TF ","."$filename";
+  $mecab = MeCab::Tagger->new();
+  $data = new TermExtract::MeCab;
+  
+  $node = $mecab->parseToNode($txt);
+  while ($node) {
+  #  my $id       = $node->{'id'};
+    my $word     = $node->{'surface'};
+    my $feature  = $node->{'feature'};
+    my @features = split(',', $feature);
+    #my @categories  = grep { $_ ne '*' } @features[0..3];    # 品詞
+    #my $conjugation = $features[4] if ($features[4] ne '*'); # 活用
+    #my $conjugate   = $features[5] if ($features[5] ne '*'); # 活用形
+    #my $description = $features[6] if ($features[6] ne '*'); # 記述
+    #print $word,$#features,"\n";
+    $word =~ tr/a-z/A-Z/; #小文字→大文字変換
+    $nom_word = Unicode::Japanese->new($word)->h2z->get; #半角→全角に統一
+    $lower_word = Unicode::Japanese->new($word)->z2h->get; #半角→全角に統一
+
+    #my $pronunce    = $features[8] if ($features[8] ne '*'); # 発音
+    #print join("\t", $word, $categories[0], $kana, $pronunce), "\n";
+    
+    #print OUT join("\t",$word,$feature),"\n";
+    $pos_str .= join(",",$word,$feature)."\n";
+    $node = $node->{next};
+  }
+  #形態素解析結果出力
+  print POS $pos_str;
+  #TermExtract 出力
+    @noun_list = $data->get_imp_word($pos_str,'var');
+    foreach (@noun_list) {
+       # 日付・時刻は表示しない
+       next if $_->[0] =~ /^(昭和)*(平成)*(\d+年)*(\d+月)*(\d+日)*(午前)*(午後)*(\d+時)*(\d+分)*(\d+秒)*$/;
+       # 数値のみは表示しない
+       next if $_->[0] =~ /^\d+$/;
+       # 結果表示
+#       printf TRM "%-60s %16.2f\n", $_->[0], $_->[1];
+       printf TRM "%-s,%.2f\n",$_->[0],$_->[1];
+    }
+    $fileid += 1;
+}
