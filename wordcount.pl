91c71d5e8162b40a384cb66c3f95e506086641e6
shooter@office <10005239@j4002468.opt.olympus.co.jp>
2012-03-29 15:29:29 +0900

    引数に取るファイルを形態素解析ファイルに変更。

--- wordcount.pl
+++ wordcount.pl
@@ -12,82 +12,64 @@ my %filewordmatrix; #{file=>{word => count}}のハッシュ
 my %filetitle;
 my $fileid=0;
 my $regex_suffix = qw/\.[^\.]+$/; #拡張子をのぞくための正規表現
-my $DELLSTR="DELLWORD"; #削除辞書区別フラグ
+our $DELLSTR="DELLWORD"; #削除辞書区別フラグ
+
+if($#ARGV == -1){
+  die "Usage:$0 [posfile]...\n";
+}
+
+#形態素解析結果を受け取り、カウント対象単語を返す
+sub IsCountWord() { 
+  my @features = split(/\t|,/,$_);
+  my $word = shift @features;
+  my $lower_word = Unicode::Japanese->new($word)->z2h->get;
+
+  if (($features[0] eq '名詞')
+       && ($features[1] !~ m/数|接尾|代名詞|固有名詞|非自立|サ変接続|副詞可能|形容動詞語幹/) 
+       && ($features[2] !~ m/助動詞語幹|助数詞/)
+       && ($lower_word !~ m/[a-zA-Z]/) 
+       && ($features[9] ne $DELLSTR) ){
+        #アルファベット一文字は削除
+        #未知語などは原型が*になるためそれ以外の場合に代入
+         if($features[6] ne "*"){ 
+           $word = $features[6];
+         }
+       }else{
+         $word = ""; #カウントしない場合はNULLを返す
+       }
+       $word =~ tr/a-z/A-Z/; #小文字→大文字変換
+       #$word = Unicode::Japanese->new($word)->h2z->get; #半角→全角に統一
+       return $word;
+}
 
 open(ALL,">allwords.txt");
 open(CNT,">arrt_words_cnt.txt");
 open(TF,">arrt_words_tf.txt");
-#open(CLUST,">arrt_words_clust.txt");
 print CNT "単語";
 print TF "単語";
 while(@ARGV){
-  my $txt = "";
   my $pos_str = "";
   my $file = shift @ARGV;
   print $file."...\n";
   my ($filename,$filepath,$filesuffix) = fileparse($file,$regex_suffix); 
   open(IN,"<$file");
-  open(TRM,">$filename"."_term.txt");
-  open(POS,">$filename"."_pos.txt");
-  $txt = do { local $/; <IN> };
-  unless($txt){
-    print "Notice:".$filename." is none...\n";
-    next;
-  }
+  #$txt = do { local $/; <IN> };
   $filetitle{$fileid}=$filename;
   print CNT ","."$filename";
   print TF ","."$filename";
-  #$mecab = MeCab::Tagger->new("-u /Users/shooter/Bin/MeCab/wikipedia.dic");
-  $mecab = MeCab::Tagger->new();
-  $data = new TermExtract::MeCab;
   
-  $node = $mecab->parseToNode($txt);
-  while ($node) {
-  #  my $id       = $node->{'id'};
-    my $word     = $node->{'surface'};
-    my $feature  = $node->{'feature'};
-    my @features = split(',', $feature);
-    #my @categories  = grep { $_ ne '*' } @features[0..3];    # 品詞
-    #my $conjugation = $features[4] if ($features[4] ne '*'); # 活用
-    #my $conjugate   = $features[5] if ($features[5] ne '*'); # 活用形
-    #my $description = $features[6] if ($features[6] ne '*'); # 記述
-#    print $word,$#features,"\n";
-    $word =~ tr/a-z/A-Z/; #小文字→大文字変換
-    $nom_word = Unicode::Japanese->new($word)->h2z->get; #半角→全角に統一
-    $lower_word = Unicode::Japanese->new($word)->z2h->get; #半角→全角に統一
+  while (<IN>) {
+    $pos_str = $_;
+    $nom_word = &IsCountWord($pos_str);
+    #print $nom_word;
 
-    #my $pronunce    = $features[8] if ($features[8] ne '*'); # 発音
-    #print join("\t", $word, $categories[0], $kana, $pronunce), "\n";
-    
-    #print OUT join("\t",$word,$feature),"\n";
-    $pos_str .= join("\t",$word,$feature)."\n";
-
-    if (($features[0] eq '名詞') && ($features[1] !~ m/数|接尾|代名詞|固有名詞|非自立|サ変接続|副詞可能|形容動詞語幹/) 
-       && ($features[2] !~ m/助動詞語幹|助数詞/) && ($features[9] ne $DELLSTR)
-       && ($lower_word !~ m/[a-zA-Z]/) #アルファベット一文字
-    )
-    {
-            #print join("\t",$word,$feature),"\n";
+    if ($nom_word){
             $allwords{$nom_word}+=1;
             $wordmatrix{$nom_word}{$fileid}+=1;
             $filewordcnt{$fileid}+=1;
-            #$filewordmatrix{$fileid}{$nom_word}+=1;
     }
     $node = $node->{next};
   }
-  #形態素解析結果出力
-  print POS $pos_str;
-  #TermExtract 出力
-    @noun_list = $data->get_imp_word($pos_str,'var');
-    foreach (@noun_list) {
-       # 日付・時刻は表示しない
-       next if $_->[0] =~ /^(昭和)*(平成)*(\d+年)*(\d+月)*(\d+日)*(午前)*(午後)*(\d+時)*(\d+分)*(\d+秒)*$/;
-       # 数値のみは表示しない
-       next if $_->[0] =~ /^\d+$/;
-       # 結果表示
-#       printf TRM "%-60s %16.2f\n", $_->[0], $_->[1];
-       printf TRM "%-s,%.2f\n",$_->[0],$_->[1];
-    }
     $fileid += 1;
 }
 
@@ -123,12 +105,3 @@ foreach $key (sort { $allwords{$b} <=> $allwords{$a} } keys %allwords) {
     print TF ","."$allwords{$key}";
     print TF ",".$idf;
 }
-
-
-#for($arrt_id=0;$arrt_id<$fileid;$arrt_id++){
-#  print CLUST $filetitle{$arrt_id};
-#  foreach $keyword (sort { $filewordmatrix{$arrt_id}{$a} <=> $filewordmatrix{$arrt_id}{$b} } keys %{$filewordmatrix{$arrt_id}}) {
-#    print CLUST "\t".$keyword."\t".$filewordmatrix{$arrt_id}->{$keyword};
-#  }
-#  print CLUST "\n";
-#}
